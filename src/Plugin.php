21c424b0d0d128d8bab6e6119f4b29e615a8aeb9
Xavier Mirabelli-Montan <xavier@whiteskyweb.com>
2022-04-22 12:01:26 +0100

    Bumping version

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,7 +13,7 @@ use RuntimeException;
 
 class Plugin implements PluginInterface, Capable
 {
-    const VERSION = 'dev-master';
+    const VERSION = '1.0.0-alpha2';
 
     /** @var Composer */
     protected $composer;
